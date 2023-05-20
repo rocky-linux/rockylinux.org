22a19408786fd4e79d09cb6a0d5864e307973713
Michael Kinder <me@mwk.email>
2023-05-20 00:29:26 -0400

    use next/link

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 const navigation = [
   { name: "News", href: "/news/" },
@@ -29,7 +30,7 @@ export default function Header() {
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         aria-label="Global"
       >
-        <a href="#" className="-m-1.5 p-1.5">
+        <Link href="#" className="-m-1.5 p-1.5">
           <span className="sr-only">Rocky Linux</span>
           <picture>
             <img
@@ -38,7 +39,7 @@ export default function Header() {
               alt="Rocky Linux Logo"
             />
           </picture>
-        </a>
+        </Link>
         <div className="flex lg:hidden">
           <button
             type="button"
@@ -51,17 +52,20 @@ export default function Header() {
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
           {navigation.map(item => (
-            <a
+            <Link
               key={item.name}
               href={item.href}
               className="text-sm font-semibold leading-6 text-gray-900"
             >
               {item.name}
-            </a>
+            </Link>
           ))}
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+          <Link
+            href="#"
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
             Download <span aria-hidden="true">&darr;</span>
-          </a>
+          </Link>
         </div>
       </nav>
       <Dialog
@@ -73,7 +77,7 @@ export default function Header() {
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
+            <Link href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Rocky Linux</span>
               <picture>
                 <img
@@ -82,7 +86,7 @@ export default function Header() {
                   alt="Rocky Linux Logo"
                 />
               </picture>
-            </a>
+            </Link>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -96,22 +100,22 @@ export default function Header() {
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
                 {navigation.map(item => (
-                  <a
+                  <Link
                     key={item.name}
                     href={item.href}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
               </div>
               <div className="py-6">
-                <a
+                <Link
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Download <span aria-hidden="true">&darr;</span>
-                </a>
+                </Link>
               </div>
             </div>
           </div>
