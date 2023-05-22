65bce3436b908edeeb2d6801edcc00a6a2c01f80
Michael Kinder <me@mwk.email>
2023-05-22 12:33:41 -0400

    fix font size on button

--- components/buttons/LinkButton.tsx
+++ components/buttons/LinkButton.tsx
@@ -38,7 +38,7 @@ const LinkButton = forwardRef<HTMLAnchorElement, ButtonComponent>(
           {...rest}
         >
           <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full bg-black group-hover:top-0.5 group-hover:left-0.5 duration-500 transition-all rounded-md"></span>
-          <span className="fold-bold relative inline-block h-full w-full border-2 border-black bg-white px-3 py-1 text-base font-semibold text-black transition duration-100 hover:bg-green-500 hover:text-white rounded-md">
+          <span className="fold-bold relative inline-block h-full w-full border-2 border-black bg-white px-3 py-1 text-sm font-semibold text-black transition duration-100 hover:bg-green-500 hover:text-white rounded-md">
             {children}
           </span>
         </a>
