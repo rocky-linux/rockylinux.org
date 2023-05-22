5e5c83269d2071c008bf87b421b7439a1a78d66a
Michael Kinder <me@mwk.email>
2023-05-22 12:26:41 -0400

    update button styling

--- components/buttons/Button.tsx
+++ components/buttons/Button.tsx
@@ -1,53 +1,54 @@
 "use client";
 
-import {ButtonVariant} from "@/constants/ButtonVariant";
-import {useButtonStyle} from "@/hooks/useButtonStyle";
-import {ButtonHTMLAttributes, forwardRef} from "react";
-import {classNames} from "@/utils/classNames";
+import { ButtonVariant } from "@/constants/ButtonVariant";
+import { useButtonStyle } from "@/hooks/useButtonStyle";
+import { ButtonHTMLAttributes, forwardRef } from "react";
+import { classNames } from "@/utils/classNames";
 
-import type {ButtonVariantType} from "@/types/ButtonType";
+import type { ButtonVariantType } from "@/types/ButtonType";
 
 export interface ButtonProps {
-    variant?: ButtonVariantType;
-    disabled?: boolean;
-    children?: React.ReactNode;
+  variant?: ButtonVariantType;
+  disabled?: boolean;
+  children?: React.ReactNode;
 }
 
 export type ButtonComponent = ButtonProps &
-    ButtonHTMLAttributes<HTMLButtonElement>;
+  ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button = forwardRef<HTMLButtonElement, ButtonComponent>(
-    (
-        {
-            variant = ButtonVariant.primary,
-            disabled = false,
-            className,
-            children,
-            ...rest
-        },
-        ref
-    ) => {
-        const {buttonVariantStyleClass} = useButtonStyle({
-            variant,
-        });
+  (
+    {
+      variant = ButtonVariant.primary,
+      disabled = false,
+      className,
+      children,
+      ...rest
+    },
+    ref
+  ) => {
+    const { buttonVariantStyleClass } = useButtonStyle({
+      variant,
+    });
 
-        return (
-            <button
-                ref={ref}
-                disabled={disabled}
-                aria-disabled={disabled}
-                className={classNames(className ?? "relative group", buttonVariantStyleClass)}
-                {...rest}
-            >
-                <span
-                    className="absolute top-0 left-0 mt-1 ml-1 h-full w-full bg-black group-hover:top-0.5 group-hover:left-0.5 duration-500 transition-all"></span>
-                <span
-                    className="fold-bold relative inline-block h-full w-full border-2 border-black bg-white px-3 py-1 text-base font-semibold text-black transition duration-100 hover:bg-green-500 hover:text-white">
-                        {children}
-                </span>
-            </button>
-        );
-    }
+    return (
+      <button
+        ref={ref}
+        disabled={disabled}
+        aria-disabled={disabled}
+        className={classNames(
+          className ?? "relative group",
+          buttonVariantStyleClass
+        )}
+        {...rest}
+      >
+        <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full bg-black group-hover:top-0.5 group-hover:left-0.5 duration-500 transition-all rounded-md"></span>
+        <span className="fold-bold relative inline-block h-full w-full border-2 border-black bg-white px-3 py-1 text-base font-semibold text-black transition duration-100 hover:bg-green-500 hover:text-white rounded-md">
+          {children}
+        </span>
+      </button>
+    );
+  }
 );
 
 Button.displayName = "Button";
--- components/buttons/LinkButton.tsx
+++ components/buttons/LinkButton.tsx
@@ -1,49 +1,50 @@
 "use client";
 
-import {ButtonVariant} from "@/constants/ButtonVariant";
-import {useButtonStyle} from "@/hooks/useButtonStyle";
-import {forwardRef, LinkHTMLAttributes} from "react";
-import {classNames} from "@/utils/classNames";
+import { ButtonVariant } from "@/constants/ButtonVariant";
+import { useButtonStyle } from "@/hooks/useButtonStyle";
+import { forwardRef, LinkHTMLAttributes } from "react";
+import { classNames } from "@/utils/classNames";
 
 import Link from "next/link";
 
-import type {ButtonVariantType} from "@/types/ButtonType";
+import type { ButtonVariantType } from "@/types/ButtonType";
 
 export interface ButtonProps {
-    href: string;
-    variant?: ButtonVariantType;
-    children?: React.ReactNode;
+  href: string;
+  variant?: ButtonVariantType;
+  children?: React.ReactNode;
 }
 
 export type ButtonComponent = ButtonProps &
-    LinkHTMLAttributes<HTMLAnchorElement>;
+  LinkHTMLAttributes<HTMLAnchorElement>;
 
 const LinkButton = forwardRef<HTMLAnchorElement, ButtonComponent>(
-    (
-        {href, variant = ButtonVariant.primary, className, children, ...rest},
-        ref
-    ) => {
-        const {buttonVariantStyleClass} = useButtonStyle({
-            variant,
-        });
+  (
+    { href, variant = ButtonVariant.primary, className, children, ...rest },
+    ref
+  ) => {
+    const { buttonVariantStyleClass } = useButtonStyle({
+      variant,
+    });
 
-        return (
-            <Link href={href} legacyBehavior>
-                <a
-                    ref={ref}
-                    className={classNames(className ?? "relative group", buttonVariantStyleClass)}
-                    {...rest}
-                >
-                    <span
-                        className="absolute top-0 left-0 mt-1 ml-1 h-full w-full bg-black group-hover:top-0.5 group-hover:left-0.5 duration-500 transition-all"></span>
-                    <span
-                        className="fold-bold relative inline-block h-full w-full border-2 border-black bg-white px-3 py-1 text-base font-semibold text-black transition duration-100 hover:bg-green-500 hover:text-white">
-                        {children}
-                    </span>
-                </a>
-            </Link>
-        );
-    }
+    return (
+      <Link href={href} legacyBehavior>
+        <a
+          ref={ref}
+          className={classNames(
+            className ?? "relative group rounded-md",
+            buttonVariantStyleClass
+          )}
+          {...rest}
+        >
+          <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full bg-black group-hover:top-0.5 group-hover:left-0.5 duration-500 transition-all rounded-md"></span>
+          <span className="fold-bold relative inline-block h-full w-full border-2 border-black bg-white px-3 py-1 text-base font-semibold text-black transition duration-100 hover:bg-green-500 hover:text-white rounded-md">
+            {children}
+          </span>
+        </a>
+      </Link>
+    );
+  }
 );
 
 LinkButton.displayName = "LinkButton";
