4c25abea9f1585b317ee9adc88d89d91adc9a756
Michael Kinder <me@mwk.email>
2023-05-22 12:27:04 -0400

    clean up hero component

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -1,38 +1,15 @@
 import { ChevronRightIcon } from "@heroicons/react/20/solid";
+import LinkButton from "@/components/buttons/LinkButton";
+import Pill from "@/components/pills/Pill";
 
 export default function Hero() {
   return (
     <div className="relative isolate overflow-hidden bg-white">
-      <svg
-        className="absolute inset-0 -z-10 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
-        aria-hidden="true"
-      >
-        <defs>
-          <pattern
-            id="0787a7c5-978c-4f66-83c7-11c213f99cb7"
-            width={200}
-            height={200}
-            x="50%"
-            y={-1}
-            patternUnits="userSpaceOnUse"
-          >
-            <path d="M.5 200V.5H200" fill="none" />
-          </pattern>
-        </defs>
-        <rect
-          width="100%"
-          height="100%"
-          strokeWidth={0}
-          fill="url(#0787a7c5-978c-4f66-83c7-11c213f99cb7)"
-        />
-      </svg>
       <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
         <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl lg:flex-shrink-0 lg:pt-8">
           <div className="mt-24 sm:mt-32 lg:mt-16">
             <a href="#" className="inline-flex space-x-6">
-              <span className="rounded-full bg-green-600/10 px-3 py-1 text-sm font-semibold leading-6 text-green-600 ring-1 ring-inset ring-green-600/10">
-                What&apos;s New
-              </span>
+              <Pill>What&apos;s New</Pill>
               <span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-600">
                 <span>Version 9.2 Released</span>
                 <ChevronRightIcon
@@ -65,12 +42,7 @@ export default function Hero() {
             and reliability.
           </p>
           <div className="mt-10 flex items-center gap-x-6">
-            <a
-              href="#"
-              className="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
-            >
-              Download
-            </a>
+            <LinkButton href="/download">Download</LinkButton>
             <a
               href="#"
               className="text-sm font-semibold leading-6 text-gray-900"
@@ -81,14 +53,17 @@ export default function Hero() {
         </div>
         <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
           <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
-            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-              <img
-                src="/images/rocky9.png"
-                alt="App screenshot"
-                width={2432}
-                height={1442}
-                className="w-[76rem] rounded-md shadow-2xl ring-1 ring-gray-900/10"
-              />
+            <div className="relative">
+              <picture>
+                <span className="absolute top-1 left-1 mt-1 ml-1 h-full w-full bg-black rounded-md"></span>
+                <img
+                  src="/images/rocky9.png"
+                  alt="App screenshot"
+                  width={2432}
+                  height={1442}
+                  className="w-[76rem] fold-bold rounded-md relative inline-block h-full border-2 border-black/70 bg-white text-base font-semibold text-black transition duration-100 hover:bg-green-500 hover:text-white"
+                />
+              </picture>
             </div>
           </div>
         </div>
