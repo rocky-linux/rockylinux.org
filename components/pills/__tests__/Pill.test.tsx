2d0d9595f102e00d1103037ed1c48959e0205b81
Michael Kinder <me@mwk.email>
2023-05-22 12:36:37 -0400

    fix pill component test

--- components/pills/__tests__/Pill.test.tsx
+++ components/pills/__tests__/Pill.test.tsx
@@ -1,11 +1,23 @@
-import { render, screen } from "@testing-library/react";
-
+import React from "react";
+import { render } from "@testing-library/react";
 import Pill from "../Pill";
 
 describe("Pill Component", () => {
-  it("renders with passed children", () => {
-    render(<Pill>Find Me!</Pill>);
+  it("renders with default props", () => {
+    const { getByText } = render(<Pill>Test Pill</Pill>);
+    const pillElement = getByText("Test Pill");
+    expect(pillElement).toBeInTheDocument();
+    expect(pillElement).toHaveClass(
+      "rounded-md bg-green-500/10 px-3 py-1 text-sm font-semibold leading-6 text-green-500 ring-1 ring-inset ring-green-500/10"
+    );
+  });
 
-    expect(screen.getByRole("span", { name: /Find Me!/i })).toBeInTheDocument();
+  it("renders with custom className", () => {
+    const { getByText } = render(
+      <Pill className="custom-class">Custom Pill</Pill>
+    );
+    const pillElement = getByText("Custom Pill");
+    expect(pillElement).toBeInTheDocument();
+    expect(pillElement).toHaveClass("custom-class");
   });
 });
