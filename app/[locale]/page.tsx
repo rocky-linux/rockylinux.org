257ee055c93a4e82fdbb1122ba601946bec1b3dc
Gabriel Graves <nebraskacoder@gmail.com>
2023-08-18 02:10:44 +0000

    Undo example content

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,3 @@
-import Container from "@/components/Container";
-
 export default function Home() {
-  return (
-    <main>
-      <Container className="mt-10">
-        <p>Text goes here.</p>
-      </Container>
-    </main>
-  );
+  return <main></main>;
 }
