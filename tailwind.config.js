7adc5fa362d607602df9b03a88e8859571cab361
Gabriel Graves <nebraskacoder@gmail.com>
2023-09-15 01:50:09 +0000

    Add Shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
   darkMode: "class",
   plugins: [require("kutty")],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     colors: {
       green: {
         50: "#b7ead9",
@@ -56,6 +63,21 @@ module.exports = {
         sans: ["Red Hat Text", "sans-serif"],
         mono: ["Red Hat Mono", "mono"],
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
+  plugins: [require("tailwindcss-animate")],
 };
