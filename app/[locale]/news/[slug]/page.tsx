929c458242871831053a81064b5347cba644ad7f
Michael Kinder <me@mwk.email>
2024-02-08 06:54:18 -0500

    Add styling for prose text

--- app/[locale]/news/[slug]/page.tsx
+++ app/[locale]/news/[slug]/page.tsx
@@ -29,17 +29,19 @@ export default async function Post({ params }: Props) {
 
   return (
     <>
-      <div className="mx-auto max-w-3xl text-base leading-7 my-24">
-        <p className="text-base font-semibold leading-7 text-primary text-center uppercase font-display">
-          <Date dateString={postData.date} />
-        </p>
-        <h1 className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl mb-12 text-center font-display">
-          {postData.title}
-        </h1>
-        <div
-          className="prose dark:prose-invert max-w-none"
-          dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
-        />
+      <div className="py-24 sm:py-32">
+        <div className="mx-auto max-w-3xl text-base leading-7">
+          <p className="text-base font-semibold leading-7 text-primary text-center uppercase font-display">
+            <Date dateString={postData.date} />
+          </p>
+          <h1 className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl mb-12 text-center font-display">
+            {postData.title}
+          </h1>
+          <div
+            className="prose dark:prose-invert prose-headings:font-display prose-a:text-primary max-w-none"
+            dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+          />
+        </div>
       </div>
     </>
   );
