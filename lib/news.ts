806030ec6c3142201d0034ed49f1881f4831b1da
Gabriel Graves <nebraskacoder@gmail.com>
2024-02-12 22:07:01 +0000

    Refactor news.ts and add remarkUtils module

--- lib/news.ts
+++ lib/news.ts
@@ -2,31 +2,28 @@ import fs from "fs";
 import path from "path";
 
 import matter from "gray-matter";
-import { unified } from "unified";
-import remarkParse from "remark-parse";
-import remarkRehype from "remark-rehype";
-import rehypeStringify from "rehype-stringify";
-import rehypeRaw from "rehype-raw";
-import remarkGfm from "remark-gfm";
+import { processMarkdownAsHTML } from "@/utils/remarkUtils";
 
 const postsDirectory = path.join(process.cwd(), "news");
 
-export function getSortedPostsData() {
-  const fileNames = fs.readdirSync(postsDirectory);
+export async function getSortedPostsData() {
+  const fileNames = await fs.promises.readdir(postsDirectory);
 
-  const allPostsData = fileNames.map((filename) => {
-    const slug = filename.replace(/\.md$/, "");
+  const allPostsData = await Promise.all(
+    fileNames.map(async (filename) => {
+      const slug = filename.replace(/\.md$/, "");
 
-    const fullPath = path.join(postsDirectory, filename);
-    const fileContents = fs.readFileSync(fullPath, "utf8");
+      const fullPath = path.join(postsDirectory, filename);
+      const fileContents = await fs.promises.readFile(fullPath, "utf8");
 
-    const matterResult = matter(fileContents);
+      const matterResult = matter(fileContents);
 
-    return {
-      slug,
-      ...(matterResult.data as { date: string; title: string }),
-    };
-  });
+      return {
+        slug,
+        ...(matterResult.data as { date: string; title: string }),
+      };
+    })
+  );
 
   return allPostsData.sort((a, b) => {
     if (a.date < b.date) {
@@ -37,8 +34,8 @@ export function getSortedPostsData() {
   });
 }
 
-export function getAllPostSlugs() {
-  const fileNames = fs.readdirSync(postsDirectory);
+export async function getAllPostSlugs() {
+  const fileNames = await fs.promises.readdir(postsDirectory);
 
   return fileNames.map((fileName) => {
     return {
@@ -51,18 +48,16 @@ export function getAllPostSlugs() {
 
 export async function getPostData(slug: string) {
   const fullPath = path.join(postsDirectory, `${slug}.md`);
+
   const fileContents = await fs.promises.readFile(fullPath, "utf8");
+
+  if (!fileContents) {
+    throw new Error(`Post with slug "${slug}" does not exist.`);
+  }
 
   const matterResult = matter(fileContents);
 
-  const processedContent = await unified()
-    .use(remarkParse)
-    .use(remarkGfm)
-    .use(remarkRehype, { allowDangerousHtml: true })
-    .use(rehypeRaw)
-    .use(rehypeStringify)
-    .process(matterResult.content);
-  const contentHtml = processedContent.toString();
+  const contentHtml = await processMarkdownAsHTML(matterResult.content);
 
   return {
     slug,
