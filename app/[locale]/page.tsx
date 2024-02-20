6799f1186bad269e6ca8d882a5abbfabb447001d
Michael Kinder <me@mwk.email>
2024-02-20 06:01:58 -0500

    Add NewsSnippet and PartnersSponsors components

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,15 @@
 import Hero from "./Hero";
 import FeatureOne from "./FeatureOne";
+import NewsSnippet from "./NewsSnippet";
+import PartnersSponsors from "./PartnersSponsors";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <FeatureOne />
+      <NewsSnippet />
+      <PartnersSponsors />
     </>
   );
 }
