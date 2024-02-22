62585d8bd865ec4d82a9e69160ac765c6e505897
Michael Kinder <me@mwk.email>
2024-02-22 04:21:47 -0500

    rollback change to prevent conflict

--- app/[locale]/Footer.tsx
+++ app/[locale]/Footer.tsx
@@ -95,7 +95,7 @@ export default function Footer() {
           >
             <g
               fill="none"
-              fillRule="evenodd"
+              fill-rule="evenodd"
             >
               <path
                 fill="currentColor"
