dbae6823e627696c3a8c38c431cc4c5fc403b4ef
Gabriel Graves <nebraskacoder@gmail.com>
2024-06-23 01:34:10 +0000

    feat: Add validation to skip Storybook Markdown filenames

--- scripts/precomit-check-md-filenames.js
+++ scripts/precomit-check-md-filenames.js
@@ -2,9 +2,11 @@ const path = require("path");
 
 process.argv.slice(2).forEach((file) => {
   const basename = path.basename(file, path.extname(file));
-  if (basename.includes(".")) {
+  // Check if the file is a Storybook Markdown file
+  const isStorybookMarkdown = basename.endsWith(".stories");
+  if (basename.includes(".") && !isStorybookMarkdown) {
     console.error(
-      `Invalid filename: ${file}. Filenames for .md and .mdx files should not contain dots.`
+      `Invalid filename: ${file}. Filenames for .md and .mdx files should not contain dots, except for Storybook Markdown files (e.g., .stories.md or .stories.mdx).`
     );
     process.exit(1);
   }
