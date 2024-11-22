a129b718c6523321bca8ea16c1a1801b5b4bfe2e
Brian Clemens <brian@tiuxo.com>
2024-11-22 13:21:04 +0900

    Add Bluesky to share buttons

--- components/shareButtons/ShareButtons.tsx
+++ components/shareButtons/ShareButtons.tsx
@@ -10,6 +10,7 @@ interface ShareButtonsProps {
 const ShareButtons = ({ url }: ShareButtonsProps) => {
   const t = useTranslations("share");
 
+  const blueskyLink = `https://bsky.app/intent/compose?text=${url}`;
   const facebookLink = `https://www.facebook.com/sharer/sharer.php?u=${url}`;
   const xLink = `https://twitter.com/intent/tweet?text=${url}`;
   const linkedInLink = `https://www.linkedin.com/sharing/share-offsite/?url=${url}`;
@@ -19,6 +20,20 @@ const ShareButtons = ({ url }: ShareButtonsProps) => {
     <>
       <h3 className="text-sm text-center mb-2">{t("shareName")}</h3>
       <div className="flex space-x-6 justify-center items-center mb-12">
+        <Link
+          href={blueskyLink}
+          className="hover:text-primary"
+          target="_blank"
+        >
+          <span className="sr-only">{t("bluesky")}</span>
+          <svg
+            fill="currentColor"
+            viewBox="0 0 600 530"
+            className="h-5 w-5"
+          >
+            <path d="m135.72 44.03c66.496 49.921 138.02 151.14 164.28 205.46 26.262-54.316 97.782-155.54 164.28-205.46 47.98-36.021 125.72-63.892 125.72 24.795 0 17.712-10.155 148.79-16.111 170.07-20.703 73.984-96.144 92.854-163.25 81.433 117.3 19.964 147.14 86.092 82.697 152.22-122.39 125.59-175.91-31.511-189.63-71.766-2.514-7.3797-3.6904-10.832-3.7077-7.8964-0.0174-2.9357-1.1937 0.51669-3.7077 7.8964-13.714 40.255-67.233 197.36-189.63 71.766-64.444-66.128-34.605-132.26 82.697-152.22-67.108 11.421-142.55-7.4491-163.25-81.433-5.9562-21.282-16.111-152.36-16.111-170.07 0-88.687 77.742-60.816 125.72-24.795z" />
+          </svg>
+        </Link>
         <MastodonDialog
           invalidUrlMsg={t("mastodon.url-valid")}
           urlMsg={t("mastodon.url")}
